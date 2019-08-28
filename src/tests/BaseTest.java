0ca20302cd5b08c665b3b112c12d9ce2d99de452
Kevin Faller <kevin.faller@abstracta.com.uy>
2019-08-27 23:12:22 -0300

    probando con ChromeOptions

--- src/tests/BaseTest.java
+++ src/tests/BaseTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,7 +17,10 @@ public abstract class BaseTest {
   @BeforeMethod
   public void setUp() {
 	  //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64/chromedriver.exe");
-	  driver= new ChromeDriver();
+	  ChromeOptions options = new ChromeOptions();
+	  options.setExperimentalOption("useAutomationExtension", false);
+	  driver = new ChromeDriver(options);
+	 // driver= new ChromeDriver();
 	  driver.get("http://opencart.abstracta.us");
 	  homePage= PageFactory.initElements(driver, CommonHome.class);
   }
