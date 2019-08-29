0e760cbcdf7036cc82f01277cd39eab8cef01806
Kevin Faller <kevin.faller@abstracta.com.uy>
2019-08-29 12:17:00 -0300

    dejar solo el argumento "--no-sandbox"

--- src/tests/BaseTest.java
+++ src/tests/BaseTest.java
@@ -10,27 +10,27 @@ import org.testng.annotations.BeforeMethod;
 import pages.CommonHome;
 
 public abstract class BaseTest {
-  
+
 	protected WebDriver driver;
 	protected CommonHome homePage;
-	
-  @BeforeMethod
-  public void setUp() {
-	  System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64/chromedriver.exe");
-	  ChromeOptions options = new ChromeOptions();
-	  options.addArguments("--headless");
-	  options.addArguments("--no-sandbox");
-	  options.addArguments("--disable-dev-shm-usage");
-	//  options.setExperimentalOption("useAutomationExtension", false);
-	  driver = new ChromeDriver(options);
-	//  driver= new ChromeDriver();
-	  driver.get("http://opencart.abstracta.us");
-	  homePage= PageFactory.initElements(driver, CommonHome.class);
-  }
 
-  @AfterMethod
-  public void tearDown() {
-	  driver.quit();
-  }
+	@BeforeMethod
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		// options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		// options.addArguments("--disable-dev-shm-usage");
+		// options.setExperimentalOption("useAutomationExtension", false);
+		driver = new ChromeDriver(options);
+		// driver= new ChromeDriver();
+		driver.get("http://opencart.abstracta.us");
+		homePage = PageFactory.initElements(driver, CommonHome.class);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
 
 }
