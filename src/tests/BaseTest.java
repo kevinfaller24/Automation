4edd5b6f646a14f2b99bea196b9a39b6717a3e61
Kevin Faller <kevin.faller@abstracta.com.uy>
2019-08-30 12:01:51 -0300

    agregar headless

--- src/tests/BaseTest.java
+++ src/tests/BaseTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,7 +17,9 @@ public abstract class BaseTest {
 	@BeforeMethod
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64/chromedriver");
-		driver = new ChromeDriver();
+		ChromeOptions options= new ChromeOptions();
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
 		driver.get("http://opencart.abstracta.us");
 		homePage = PageFactory.initElements(driver, CommonHome.class);
 	}
