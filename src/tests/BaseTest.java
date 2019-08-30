c2a8382a9bd6802ebc6406d9afae6a5c1b9410cb
Kevin Faller <kevin.faller@abstracta.com.uy>
2019-08-30 11:53:56 -0300

    ultimo

--- src/tests/BaseTest.java
+++ src/tests/BaseTest.java
@@ -2,7 +2,6 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,14 +15,8 @@ public abstract class BaseTest {
 
 	@BeforeMethod
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "drivers//chromedriver_linux64//chromedriver");
-		//ChromeOptions options = new ChromeOptions();
-		//options.addArguments("--headless");
-		//options.addArguments("--no-sandbox");
-		//options.addArguments("--disable-dev-shm-usage");
-		// options.setExperimentalOption("useAutomationExtension", false);
-		//driver = new ChromeDriver(options);
-		 driver= new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64/chromedriver");
+		driver = new ChromeDriver();
 		driver.get("http://opencart.abstracta.us");
 		homePage = PageFactory.initElements(driver, CommonHome.class);
 	}
